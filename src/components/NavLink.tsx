fe0a93450ed7364ed4811bb13f8c69b58aca9cda
Karan-developer774 <karanmouryakaran774@gmail.com>
2026-02-19 17:02:39 +0530

    fixes

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -2,33 +2,27 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
 
 interface NavLinkProps {
   href: string;
+  children: React.ReactNode;
   className?: string;
   activeClassName?: string;
-  children: React.ReactNode;
 }
 
-const NavLink = forwardRef<HTMLAnchorElement, NavLinkProps>(
-  ({ href, className, activeClassName, children, ...props }, ref) => {
-    const pathname = usePathname();
-    const isActive = pathname === href;
+export function NavLink({
+  href,
+  children,
+  className,
+  activeClassName,
+}: NavLinkProps) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
 
-    return (
-      <Link
-        ref={ref}
-        href={href}
-        className={cn(className, isActive && activeClassName)}
-        {...props}>
-        {children}
-      </Link>
-    );
-  },
-);
-
-NavLink.displayName = "NavLink";
-
-export { NavLink };
+  return (
+    <Link href={href} className={cn(className, isActive && activeClassName)}>
+      {children}
+    </Link>
+  );
+}
